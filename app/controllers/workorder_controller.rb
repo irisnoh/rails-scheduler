8ad48dd382d65e82cc703c9d79028bc7fb70c9d1
irisnoh <53018217+irisnoh@users.noreply.github.com>
2020-11-03 17:39:44 -0600

    add bulma

--- app/controllers/workorder_controller.rb
+++ app/controllers/workorder_controller.rb
@@ -1,2 +1,7 @@
 class WorkorderController < ApplicationController
+
+    def index
+        @workorders = Workorder.all.order('id asc')
+        # @technicians = Technician.includes(:location).includes(:technician).select("location_id AS location.id")
+      end
 end
