dbfec04212dc69170158d759d21842c7b1285e96
irisnoh <53018217+irisnoh@users.noreply.github.com>
2020-11-04 14:41:55 -0600

    add fullcalendar timegrid

--- app/javascript/fullcalendar/fullcalendar.js
+++ app/javascript/fullcalendar/fullcalendar.js
@@ -1,15 +1,25 @@
 import { Calendar } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 
 
-document.addEventListener('turbolinks:load', function() {
-  var calendarEl = document.getElementById('calendar');
+document.addEventListener('turbolinks:load', function () {
+    var calendarEl = document.getElementById('calendar');
+    let calendar = new Calendar(calendarEl, {
+        plugins: [ dayGridPlugin, timeGridPlugin ],
+        initialView: 'timeGridWeek'
+      });
+    // var calendar = new Calendar(calendarEl, {
+       
+    //     plugins: [dayGridPlugin, timeGridPlugin],
+    //     views: {
+    //         timeGridFourDay: {
+    //             type: 'timeGrid',
+    //             duration: { days: 4 },
+    //         }
+    //     },
 
-  var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin ],
-    height: "parent",
+    // });
 
-  });
-
-  calendar.render();
+    calendar.render();
 });
